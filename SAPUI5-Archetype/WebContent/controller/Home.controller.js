9b67f9779e6d88cca1c0e5ee422e8ef4d9a25966
Raprins <raprins@example.com>
2017-06-16 14:42:15 +0200

    Archetype Init

---
+++ SAPUI5-Archetype/WebContent/controller/Home.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"com/raprins/archetype/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+	return BaseController.extend("com.raprins.archetype.controller.Home", {
+		onInit: function () {
+		}
+	});
+});
---
+++ SAPUI5-Archetype/WebContent/controller/NotFound.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"com/raprins/archetype/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+	return BaseController.extend("com.raprins.archetype.controller.NotFound", {
+		onInit: function () {
+			
+		},
+		
+		back : function(){
+			
+		}
+		
+	});
+});
