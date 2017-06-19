f53a301b81326c98871f8537cf02b9b3ac8bba62
Raprins <raprins@example.com>
2017-06-19 09:36:46 +0200

    19/06/2017

--- Music/WebContent/controller/Home.controller.js
+++ Music/WebContent/controller/Home.controller.js
@@ -1,13 +1,28 @@
 sap.ui.define([
 	"com/raprins/music/controller/BaseController",
-], function (Controller) {
+	"sap/ui/model/json/JSONModel"
+], function (Controller,JSONModel) {
 	"use strict";
+	
 	return Controller.extend("com.raprins.music.controller.Home", {
+		/**
+		 * Init
+		 */
 		onInit: function () {
 			
+			this.oModelMusics = new JSONModel();
+			this.oModelMusics.attachRequestCompleted(this._onDataCompleted());
+			
+			this.oModelMusics.loadData("https://itunes.apple.com/search?term=a");
 		},
 		
-		navToMusic : function(){
+		
+		/**
+		 * Fetch Data 
+		 */
+		_onDataCompleted : function(){
+			
+			this.getView().setModel(this.oModelMusics, "musics");
 			
 		}
 	});
