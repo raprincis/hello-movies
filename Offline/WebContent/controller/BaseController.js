e724a8a9e9e22da01e5a570ddc34f5ec014d7aeb
RaPrincis <raprincis@example.com>
2017-08-02 16:57:29 +0200

    A

--- Offline/WebContent/controller/BaseController.js
+++ Offline/WebContent/controller/BaseController.js
@@ -1,11 +1,23 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/routing/History"
-], function (Controller, History) {
+	"sap/ui/core/routing/History",
+	"com/raprins/ui5/offline/model/JSONModel"
+], function (Controller, History, JSONModel) {
 	"use strict";
 	return Controller.extend("com.raprins.ui5.offline.controller.BaseController", {
 		getRouter : function () {
 			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		
+		
+		getModel : function(){
+			
+			if(!this.oModel){
+				this.oModel = new JSONModel();
+			}
+			
+			return this.oModel;
+			
 		},
 		
 		
--- Offline/WebContent/controller/Home.controller.js
+++ Offline/WebContent/controller/Home.controller.js
@@ -1,15 +1,53 @@
 sap.ui.define([
-	"com/raprins/ui5/offline/controller/BaseController"
-], function (BaseController) {
+	"com/raprins/ui5/offline/controller/BaseController",
+	"sap/m/Dialog"
+], function (BaseController, Dialog) {
 	"use strict";
 	return BaseController.extend("com.raprins.ui5.offline.controller.Home", {
 		onInit: function () {
 			
+			this.getModel().setData([]);
+			this.oPersonModel = new sap.ui.model.json.JSONModel();
+			this.getModel().attachDataChanged(this._onDataChanged.bind(this));
+			this.getView().setModel(this.getModel(),"personnes");
+		},
+		
+		onAddPersonne : function(oEvent){
+			this.oPersonModel.setData({});
+			var oPersonFormDialog = this._getPersonForm();
+			oPersonFormDialog.setModel(this.oPersonModel,"personForm");
+			oPersonFormDialog.open();
+		
+		},
+		
+		_onDataChanged : function(oEvent){
 			
+			this._getPersonForm().setModel(null,"personForm");
+			this._getPersonForm().close();
 			
+		},
+		
+		_getPersonForm : function(){
 			
+			if(!this._oPersonForm){
+				this._oPersonForm = sap.ui.xmlfragment("com.raprins.ui5.offline.view.PersonForm", this);
+				this.getView().addDependent(this._oPersonForm);
+			}
 			
+			return this._oPersonForm;
+		},
+		
+		onSavePersonne : function(oEvent){
+			var oModel = oEvent.getSource().getModel("personForm");
+			this.getModel().addData(oModel.getData());
 			
+		},
+		
+		onClosePersonForm : function(){
+			this._getPersonForm().close();
 		}
+		
+	
+	
 	});
 });
